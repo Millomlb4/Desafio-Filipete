4ac0abb7d446cba43313711fcdb00d30f399cb83
Millomlb4 <romulocosta697@gmail.com>
2026-03-05 17:43:06 -0300

    mudança de planos

---
+++ index.js
@@ -0,0 +1,80 @@
+let nome_aventureiro = "Tryckell"
+
+let quantidade_xp = 2001
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+if(quantidade_xp <= 1000){
+
+    console.log(`O héroi de nome ${nome_aventureiro}, está no nível de Ferro`)
+}
+else if(quantidade_xp >= 1001){
+
+    console.log(`O héroi de nome ${nome_aventureiro}, está no nível de Bronze`)
+}
+else if(quantidade_xp <=2000){
+
+    console.log(`O héroi de nome ${nome_aventureiro}, está no nível de Bronze`)
+}
+else if(quantidade_xp >= 2001){
+
+    console.log(`O héroi de nome ${nome_aventureiro}, está no nível de Prata`)
+}
+else if(quantidade_xp <= 5000){
+
+    console.log(`O héroi de nome ${nome_aventureiro}, está no nível de Prata`)
+}
+else if(quantidade_xp >= 5001){
+
+    console.log(`O héroi de nome ${nome_aventureiro}, está no nível de Ouro`)
+}
+else if(quantidade_xp <= 7000){
+
+    console.log(`O héroi de nome ${nome_aventureiro}, está no nível de Ouro`)
+}
+else if(quantidade_xp >= 7001){
+
+    console.log(`O héroi de nome ${nome_aventureiro}, está no nível de Platina`)
+}
+else if(quantidade_xp <= 8000){
+
+    console.log(`O héroi de nome ${nome_aventureiro}, está no nível de Platina`)
+}
+else if(quantidade_xp >= 8001){
+
+    console.log(`O héroi de nome ${nome_aventureiro}, está no nível de Ascendente`)
+}
+else if(quantidade_xp <= 9000){
+
+    console.log(`O héroi de nome ${nome_aventureiro}, está no nível de Ascendente`)
+}
+else if(quantidade_xp >= 9001){
+
+    console.log(`O héroi de nome ${nome_aventureiro}, está no nível de Imortal`)
+}
+else if(quantidade_xp <= 10000){
+
+    console.log(`O héroi de nome ${nome_aventureiro}, está no nível de Imortal`)
+}
+else if (quantidade_xp >= 10001){
+
+    console.log(`O héroi de nome ${nome_aventureiro}, está no nível de Radiante`)
+}
